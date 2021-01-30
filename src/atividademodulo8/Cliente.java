cca3401430aec844dcaa533a9ee3334fb28f5a94
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-01-30 14:46:48 -0300

    Adicionando métodos Getters e Setters

--- src/atividademodulo8/Cliente.java
+++ src/atividademodulo8/Cliente.java
@@ -10,5 +10,15 @@ public class Cliente {
     public Cliente(String nome){
         this.nome = nome;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
     
 }
