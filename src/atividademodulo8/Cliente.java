0b992d7d9573a39ef0c36f65af4f86681cd6396e
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-01-30 08:49:53 -0300

    Criando class Cliente

---
+++ src/atividademodulo8/Cliente.java
@@ -0,0 +1,14 @@
+package atividademodulo8;
+
+/**
+ *
+ * @author Victor
+ */
+public class Cliente {
+    public String nome;
+    
+    public Cliente(String nome){
+        this.nome = nome;
+    }
+    
+}
