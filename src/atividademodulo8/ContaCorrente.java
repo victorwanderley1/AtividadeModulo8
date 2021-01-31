5ebc76c1c924ffc08013cb2a16bd45c57001bb5c
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-01-31 12:28:40 -0300

    Adicionado método sacar

--- src/atividademodulo8/ContaCorrente.java
+++ src/atividademodulo8/ContaCorrente.java
@@ -15,7 +15,40 @@ public class ContaCorrente extends Conta implements Tributavel{
 
     @Override
     public double getSaldo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.saldo + chequeEspecial;
     }
     
+    public boolean sacar(double valorSaque){
+        switch (verificarPossibilidadeSaque(valorSaque)){
+            case 1:
+                this.saldo -= valorSaque;
+                return true;
+                
+            case 2:
+                valorSaque -= this.saldo;
+                this.saldo = 0;
+                chequeEspecial -= valorSaque;
+                return true;
+                
+            default: 
+                System.out.println("Saldo Insuficiente!");
+                return false;
+        }
+    }
+    
+    public int verificarPossibilidadeSaque(double valorSaque){
+        if (valorSaque <= this.saldo) return 1;
+        else if (valorSaque <= getSaldo()) return 2;
+        else return 3;
+    }
+
+    @Override
+    public double valorImpostoTransferencia() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double valorImpostoManutencaoDaConta() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
