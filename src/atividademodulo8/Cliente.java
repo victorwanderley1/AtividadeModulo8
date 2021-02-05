58a6c8794d5fa22c24fa9e321eb8fcb464429b17
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-02-05 18:02:18 -0300

    Adicionando lista de contas e métodos getters e setters

--- src/atividademodulo8/Cliente.java
+++ src/atividademodulo8/Cliente.java
@@ -1,11 +1,15 @@
 package atividademodulo8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Victor
  */
 public class Cliente {
     public String nome;
+    List<Conta> listaDeContas = new ArrayList();
     
     public Cliente(String nome){
         this.nome = nome;
@@ -18,7 +22,13 @@ public class Cliente {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    
-    
+
+    public List<Conta> getListaDeContas() {
+        return listaDeContas;
+    }
+
+    public void addConta(Conta conta) {
+        this.listaDeContas.add(conta);
+    }
+
 }
