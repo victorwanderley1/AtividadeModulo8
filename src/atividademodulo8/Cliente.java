472bce8c80bc3bec11d516406b559942c43ac838
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-02-06 06:09:12 -0300

    Adicionando métodos iniciais

--- src/atividademodulo8/Cliente.java
+++ src/atividademodulo8/Cliente.java
@@ -8,11 +8,22 @@ import java.util.List;
  * @author Victor
  */
 public class Cliente {
+    public final int CONTACORRENTE = 0;
+    public final int CONTAPOUPANCA = 1;
+    public final int CONTASALARIO = 2;
+    
+    
     public String nome;
+    private boolean verifiContaCorrente;
+    private boolean verifiContaPoupanca;
+    private boolean verifiContaSalario;
     private List<Conta> listaDeContas = new ArrayList();
     
     public Cliente(String nome){
         this.nome = nome;
+        this.verifiContaCorrente = false;
+        this.verifiContaPoupanca = false;
+        this.verifiContaSalario = false;
     }
 
     public String getNome() {
@@ -37,6 +48,67 @@ public class Cliente {
             saldoTotal += conta.getSaldo();
         }
         return saldoTotal;
+    }
+    
+    /**
+     * Cria uma instância de uma conta corrente e adiciona à listaDeContas do Cliente
+     * @param numeroAgencia
+     * @param numeroConta
+     * @param saldo
+     * @param chequeEspecial
+     * @return retorna false caso o cliente já tenha uma conta poupança
+     */
+    public boolean abrirContaCorrente(int numeroAgencia, int numeroConta, 
+            double saldo, double chequeEspecial){
+        if(!this.verifiContaPoupanca){
+            ContaCorrente contaCorrente = new ContaCorrente(this, numeroAgencia,
+                numeroConta, saldo, chequeEspecial);
+            addConta(contaCorrente);
+            this.verifiContaCorrente = false;
+            return true;
+        }else return false;
+        
+    }
+    
+    /**
+     * Cria uma instância de uma conta poupança e adiciona à listaDeContas do Cliente
+     * @param numeroAgencia
+     * @param numeroConta
+     * @param saldo
+     * @param diaAniversario
+     * @param taxaDeJuros
+     * @return retorna false caso o cliente já tenha uma conta poupança
+     */
+    public boolean abrirContaPoupanca(int numeroAgencia, int numeroConta, 
+            double saldo, int diaAniversario, double taxaDeJuros){
+        
+        if(!this.verifiContaPoupanca){
+            ContaPoupanca contaPoupanca = new ContaPoupanca(this, numeroAgencia, numeroConta,
+                saldo, diaAniversario, taxaDeJuros);
+            addConta(contaPoupanca);
+            this.verifiContaPoupanca = true;
+            return true;
+        }else return false;
+    }
+    
+    /**
+     * Cria uma instância de uma conta salário e adiciona à listaDeContas do Cliente
+     * 
+     * @param numeroAgencia
+     * @param numeroConta
+     * @param saldo
+     * @param limiteSaques
+     * @return retorna false caso o cliente já tenha uma conta salário 
+     */
+    public boolean abrirContaSalario(int numeroAgencia, int numeroConta,
+            double saldo, int limiteSaques){
+        if (!this.verifiContaSalario){
+            ContaSalario contaSalario = new ContaSalario(this, numeroAgencia,
+                    numeroConta, saldo, limiteSaques);
+            addConta(contaSalario);
+            this.verifiContaSalario = true;
+            return true;
+        } else return false;
     }
 
 }
