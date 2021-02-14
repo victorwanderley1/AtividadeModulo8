c6d5c0bb2d542dbdd05512e5d337ab83977d5ba7
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-02-14 08:31:04 -0300

    Criando melhorando métodos de relatório

--- src/atividademodulo8/MenuBanco.java
+++ src/atividademodulo8/MenuBanco.java
@@ -1,5 +1,4 @@
 package atividademodulo8;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -18,27 +17,24 @@ public class MenuBanco {
             System.out.println("Selecione uma opção abaixo:");
             System.out.println("(1) Cadastrar novo cliente");
             System.out.println("(2) Abrir nova conta");
+            System.out.println("(3) Relatórios");
             System.out.println("(0) Encerrar programa");
             System.out.print("Digite a opção: ");
             int opcao = entradaTeclado.nextInt();
             switch (opcao){
                 case 1:
-                    System.out.println("Opção 1");
                     menuCadastrarCliente();
                     break;
                 case 2:
-                    System.out.println("Opção 2");
                     menuAbrirConta();
                     break;
                 case 3:
-                    System.out.println("Opção 3");
-                    System.out.println("Clientes cadastrados no banco: ");
-                    banco.getClientesDoBanco().values().stream().map(cliente -> cliente.getNome()).sorted().forEach(System.out::println);
-                    break;
-                case 4:
-                    System.out.println("Qual relatório deseja receber? \n1 - Saldo Total Em Banco ");
+                    System.out.println("\nQual relatório deseja receber? "
+                            + "\n1) Clientes Cadastrados no banco"
+                            + "\n2) - Saldo Total Em Banco ");
                     int opcaoRelatorio = entradaTeclado.nextInt();
                     menuRelatorio(opcaoRelatorio);
+                    break;
                 case 0:
                     entradaTeclado.close();
                     System.out.println("Opção 0");
@@ -56,13 +52,13 @@ public class MenuBanco {
     }
     
     public void menuAbrirConta(){
-        System.out.println("Selecione o cliente que deseja abrir a conta: ");
-        banco.getClientesDoBanco().values().stream().map(cliente -> cliente.getNome()).forEach(System.out::println);
-        System.out.println("Digite o nome do cliente: ");
+        System.out.println("Selecione o cliente que deseja abrir a conta: \n");
+        banco.getClientesDoBanco().values().stream().map(cliente -> cliente.getNome()).sorted().forEach(System.out::println);
+        System.out.println("\nDigite o nome do cliente: ");
         String nomeCliente = entradaTeclado.next();
         if (banco.getClientesDoBanco().containsKey(nomeCliente)){
             System.out.println("\nSelecione o tipo  de conta: ");
-            System.out.println("0 - Conta Corrente\n1 - Conta Poupança\n2 - Conta Salário");
+            System.out.println("1 - Conta Corrente\n2 - Conta Poupança\n3 - Conta Salário");
             int tipoDeConta = entradaTeclado.nextInt();
             menuAberturaDeConta(nomeCliente, tipoDeConta);
         }
@@ -71,7 +67,7 @@ public class MenuBanco {
     
     private void menuAberturaDeConta(String nomeCliente, int tipoDeConta){
         switch (tipoDeConta){
-            case 0:
+            case 1:
                 System.out.println("\n\n\n");
                 System.out.println("Digite os dados do Cliente");
                 System.out.println("Número da agência: ");
@@ -84,7 +80,7 @@ public class MenuBanco {
                 double saldoChequeEspecial = entradaTeclado.nextDouble();
                 banco.getClientesDoBanco().get(nomeCliente).abrirContaCorrente(numeroAgencia, numeroConta, saldoInicial, saldoChequeEspecial);
                 break;
-            case 1:
+            case 2:
                 System.out.println("\n\n\n");
                 System.out.println("Digite os dados do Cliente");
                 System.out.println("Número da agência: ");
@@ -99,7 +95,7 @@ public class MenuBanco {
                 double valorTaxaDeJuros = entradaTeclado.nextDouble();
                 banco.getClientesDoBanco().get(nomeCliente).abrirContaPoupanca(numeroAgencia, numeroConta, saldoInicial, diaAniversario, valorTaxaDeJuros);
                 break;
-            case 2:
+            case 3:
                 System.out.println("\n\n\n");
                 System.out.println("Digite os dados do Cliente");
                 System.out.println("Número da agência: ");
@@ -121,6 +117,11 @@ public class MenuBanco {
     public void menuRelatorio(int relatorio){
         switch (relatorio){
             case 1:
+                System.out.println("Clientes cadastrados no banco: ");
+                banco.getClientesDoBanco().values().stream().map(cliente -> cliente.getNome()).sorted().forEach(System.out::println);
+                menuPrincipal();
+                break;
+            case 2:
                 List<Double> listSaldoTotalEmBanco;
                 listSaldoTotalEmBanco = banco.getClientesDoBanco().values()
                         .stream().map(cliente -> cliente.getSaldoTotalCliente())
@@ -130,6 +131,10 @@ public class MenuBanco {
                     saldoTotalEmBanco += saldo; 
                }
                 System.out.printf("\nValor total no banco: R$ %.2f \n", saldoTotalEmBanco);
+                menuPrincipal();
+                break;
+            default:
+                menuPrincipal();
         }
     }
 }
